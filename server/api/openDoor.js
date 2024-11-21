2ddd7048e71107e8c74f3468db9623e90e91eeb4
alexis <alexis.anzieu@gmail.com>
2024-11-21 15:49:28 -0500

    make door great again

--- server/api/openDoor.js
+++ server/api/openDoor.js
@@ -2,7 +2,7 @@ import { eventHandler, send } from 'h3';
 
 export default eventHandler(async (req) => {
     try {
-        const result = await fetch(`http://${process.env.STATIC_IP}:8080/api`);
+        const result = await fetch(`http://${process.env.STATIC_IP}:8081/api`);
         const data = await result.json();
         return send(req, JSON.stringify(data));
     } catch (error) {
