6a685614be6f5beca9331e9c874b8ceb83fbaca8
alexis <alexis.anzieu@gmail.com>
2025-03-29 10:36:35 -0400

    add alice door interaction with images

---
+++ pages/alice.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="fullscreen-container" @click="openDoor">
+    <transition name="door">
+      <div v-if="!isDoorOpen" class="door">
+        <img :src="doorImageUrl" alt="Door" class="fullscreen-image" />
+      </div>
+    </transition>
+    <div class="hidden-content" :class="{ visible: isDoorOpen }">
+      <img
+        :src="hiddenImageUrl"
+        alt="Hidden Content"
+        class="fullscreen-image"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const isDoorOpen = ref(false);
+const doorImageUrl = "/images/alice/door.jpeg";
+const hiddenImageUrl = "/images/alice/stairs.jpg";
+
+const openDoor = async () => {
+  if (isDoorOpen.value) {
+    return;
+  }
+  fetch("/api/openDoor", {
+    method: "GET",
+  });
+  isDoorOpen.value = true;
+};
+</script>
+
+<style scoped>
+.fullscreen-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.fullscreen-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.door {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  transform-origin: left;
+  transition: transform 1.5s ease-in-out;
+}
+
+.door-enter-active,
+.door-leave-active {
+  transform: perspective(1200px) rotateY(-105deg);
+}
+
+.hidden-content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transition: opacity 0.5s ease-in-out;
+}
+
+.hidden-content.visible {
+  opacity: 1;
+}
+</style>
